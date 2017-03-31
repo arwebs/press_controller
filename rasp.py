5856a851a7c2df6dab75c99cb05920b484dd5290
Andrew Webster <andrew.r.webster7@gmail.com>
2017-03-31 07:41:06 -0400

    starting to make things a bit more modular

--- rasp.py
+++ rasp.py
@@ -8,63 +8,103 @@ import Adafruit_GPIO.MCP230xx as MCP
 import Adafruit_MAX31855.MAX31855 as MAX31855
 import Adafruit_MCP3008
 
-
-# Define a function to convert celsius to fahrenheit.
-def c_to_f(c):
+class Utilities:
+    @staticmethod
+    def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
 
-# Define MCP pins connected to the LCD.
-lcd_rs        = 0
-lcd_en        = 1
-lcd_d4        = 2
-lcd_d5        = 3
-lcd_d6        = 4
-lcd_d7        = 5
-lcd_red       = 6
-lcd_green     = 7
-lcd_blue      = 8
+    class fake_lcd():
+        def clear(self):
+            print 'LCD Cleared'
+        def message(self,text):
+            print 'LCD Text: ' + text
 
-lcd_columns = 20
-lcd_rows    = 4
+class MainControl:
+    # program_mode = None
+    # lcd = None
+    # temperature_sensors = None
+    # analog_sensor = None
 
-gpio = MCP.MCP23017(0x22,busnum=1)
-lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                              lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue,
-                              gpio=gpio)
+    def __init__(self):
+        self.program_mode = 'Manual'
+        self.lcd = Utilities.fake_lcd() #todo remove this and enable setup_lcd()
+        # self.setup_lcd()
+        self.setup_spi_interfaces()
 
-lcd.message('Hello\nworld!')
-time.sleep(1)
+    def setup_lcd(self):
+        lcd_rs        = 0
+        lcd_en        = 1
+        lcd_d4        = 2
+        lcd_d5        = 3
+        lcd_d6        = 4
+        lcd_d7        = 5
+        lcd_red       = 6
+        lcd_green     = 7
+        lcd_blue      = 8
 
-# Raspberry Pi software SPI configuration.
-CLK = 18
-CS1  = 16
-CS2 = 12
-CSAnalog = 25
-DO  = 23
-MOSI = 24
-sensor = MAX31855.MAX31855(CLK, CS1, DO)
-sensor2 = MAX31855.MAX31855(CLK, CS2, DO)
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CSAnalog, miso=DO, mosi=MOSI)
+        lcd_columns = 20
+        lcd_rows    = 4
+
+        gpio = MCP.MCP23017(0x22,busnum=1)
+        self.lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                                      lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue,
+                                      gpio=gpio)
+    def setup_spi_interfaces(self):
+        clk = 18
+        cs_sensor_1  = 16
+        cs_sensor_2 = 12
+        cs_analog = 25
+        data_out  = 23
+        data_in = 24
+        sensor = MAX31855.MAX31855(clk, cs_sensor_1, data_out)
+        sensor2 = MAX31855.MAX31855(clk, cs_sensor_2, data_out)
+        mcp = Adafruit_MCP3008.MCP3008(clk=clk, cs=cs_analog, miso=data_out, mosi=data_in)
+
+        self.temperature_sensors = [sensor, sensor2]
+        self.analog_sensor = mcp
+
+
+    def check_program_mode(self):
+        #TODO get mode selection from input
+        print 'Checking Program State'
+
+    def set_relays(self):
+        print "setting relays"
+        #TODO set relays based on input or program state
+
+    def main_control_loop(self):
+        while self.program_mode == 'Manual':
+            self.lcd.clear()
+            for i, sensor in enumerate(self.temperature_sensors):
+                temp = sensor.readTempC()
+                internal = sensor.readInternalC()
+                print('Thermocouple {2} Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, Utilities.c_to_f(temp), i))
+                print('    Internal {2} Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, Utilities.c_to_f(internal), i))
+                self.lcd.message('T{2}:{0:0.2F}*C/{1:0.2F}*F\n'.format(internal, Utilities.c_to_f(internal), i))
+
+            values = [0] * 8
+            for sensor in range(8):
+                # The read_ad function will get the value of the specified channel (0-7).
+                values[sensor] = self.analog_sensor.read_adc(sensor)
+            print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+            self.lcd.message('Pot: {0}'.format(values[0]))
+            time.sleep(2.0)
+            self.set_relays()
+            self.check_program_mode()
+
+        while self.program_mode == 'Configure':
+            self.lcd.clear()
+
+            self.check_program_mode()
+
+        while self.program_mode == 'Automatic':
+            self.lcd.clear()
+
+            self.check_program_mode()
+
+control = MainControl()
 while True:
-    temp = sensor.readTempC()
-    internal = sensor.readInternalC()
-    print('Thermocouple 1 Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
-    print('    Internal 1 Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal)))
-    # time.sleep(1.0)
-    temp2 = sensor2.readTempC()
-    internal2 = sensor2.readInternalC()
-    print('Thermocouple 2 Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp2, c_to_f(temp2)))
-    print('    Internal 2 Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal2, c_to_f(internal2)))
-    values = [0] * 8
-    for i in range(8):
-        # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
-        # Print the ADC values.
-        print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-    lcd.clear()
-    lcd.message('T1:{0:0.2F}*C/{1:0.2F}*F\nT2:{2:0.2F}*C/{3:0.2F}*F\nPOT:{4:0.2F}'.format(internal, c_to_f(internal),internal2, c_to_f(internal2), values[0]))
-    time.sleep(2.0)
-
+    control.main_control_loop()
 
 #
 # def get_target_slope(initial_temp, target_temp, rampup):
