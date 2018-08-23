edd68404d596eac951795d0cda2a458036d1bc72
Andrew Webster <andrew.r.webster7@gmail.com>
2018-08-23 07:13:01 -0400

    added configuration state logic

--- PressControl/ConfigState.py
+++ PressControl/ConfigState.py
@@ -1,12 +1,19 @@
 import press_globals as pg
 
+
+def set_value_from_pot(pot_value, min_value, max_value):
+    return (pot_value * (max_value-min_value))/1024.0
+
+
 class ConfigState:
 
     def __init__(self):
+        self.config_step = 0
         pass
 
     def enter(self):
         print "Entering Setup State"
+        self.config_step = 0
         return self
 
     def exit(self):
@@ -14,6 +21,32 @@ class ConfigState:
         return True
 
     def in_state(self, allSensorValues, lcd, led_gpio):
+
+        if pg.start_button:
+            self.config_step = (self.config_step + 1) % 2
+
+        if self.config_step == 0:
+            pg.total_run_duration = set_value_from_pot(allSensorValues[2][1], 20., 100.)
+            pg.top_rampup_time = set_value_from_pot(allSensorValues[2][2], 20., 100.)
+            pg.bottom_rampup_time = set_value_from_pot(allSensorValues[2][3], 20., 100.)
+            lcd.message("Timing Config:\nTop Ramp-up: " +
+                        str(pg.top_rampup_time) +
+                        "\nTotal Duration: " +
+                        str(pg.total_run_duration) +
+                        "\nBottom Ramp-up: " +
+                        str(pg.bottom_rampup_time)
+                        )
+        elif self.config_step == 1:
+            pg.target_pressure = set_value_from_pot(allSensorValues[2][1], 20., 100.)
+            pg.top_max_temp = set_value_from_pot(allSensorValues[2][2], 50., 100.)
+            pg.bottom_max_temp = set_value_from_pot(allSensorValues[2][3], 50., 100.)
+            lcd.message("Temp/Press Config:\nTop max temp: " +
+                        str(pg.top_max_temp) +
+                        "\nMax pressure: " +
+                        str(pg.target_pressure) +
+                        "\nBottom max temp: " +
+                        str(pg.bottom_max_temp)
+                        )
 
         pg.top_heat_blanket_on = False
         pg.bottom_heat_blanket_on = False
--- PressControl/press_globals.py
+++ PressControl/press_globals.py
@@ -2,6 +2,7 @@ def init():
     global start_button, stop_button, auto_mode, manual_mode, config_mode, top_heat_blanket_off, top_heat_blanket_auto, top_heat_blanket_on
     global bottom_heat_blanket_off, bottom_heat_blanket_auto, bottom_heat_blanket_on, intake_solenoid_off, intake_solenoid_auto, intake_solenoid_on
     global exhaust_solenoid_off, exhaust_solenoid_auto, exhaust_solenoid_on
+    global total_run_duration, top_rampup_time, bottom_rampup_time, top_max_temp, bottom_max_temp, target_pressure
 
     start_button = False
     stop_button = False
@@ -20,3 +21,9 @@ def init():
     exhaust_solenoid_off = False
     exhaust_solenoid_auto = True
     exhaust_solenoid_on = False
+    total_run_duration = 40.
+    top_rampup_time = 20.
+    bottom_rampup_time = 20.
+    top_max_temp = 80.
+    bottom_max_temp = 80.
+    target_pressure = 80
